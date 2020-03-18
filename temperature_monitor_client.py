aca3af500ae465907e1b374934b80dc8336fdc11
BAnderson6895 <BAnderson6895@gmail.com>
2020-03-17 22:05:36 -0400

    Add use for minmax function

--- temperature_monitor_client.py
+++ temperature_monitor_client.py
@@ -7,7 +7,11 @@ def main():
     channel = grpc.insecure_channel('localhost:50051')
     stub = temperature_monitor_pb2_grpc.TemperatureMonitorStub(channel)
     response = stub.CurrentTemperature(temperature_monitor_pb2.Empty())
-    print("Client received: " + response.celsius)
+    response2 = stub.CurrentTemperature(temperature_monitor_pb2.Empty())
+    response3 = stub.MinMaxTemperature(temperature_monitor_pb2.Empty())
+    print("Client received: " + str(response.celsius))
+    print("Client received: " + str(response2.celsius))
+    print("Client received min: " + str(response3.min) + " max: " + str(response3.max))
 
 
 if __name__ == '__main__':
