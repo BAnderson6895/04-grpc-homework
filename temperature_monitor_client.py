d3916fcb1fdf449e81fa95f77dd94c2baa705ac8
BAnderson6895 <BAnderson6895@gmail.com>
2020-03-17 21:44:34 -0400

    Fix variable name in print statement

--- temperature_monitor_client.py
+++ temperature_monitor_client.py
@@ -7,7 +7,7 @@ def main():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = temperature_monitor_pb2_grpc.TemperatureMonitorStub(channel)
         response = stub.CurrentTemperature(temperature_monitor_pb2.Empty())
-    print("Client received: " + response.message)
+    print("Client received: " + response.celsius)
 
 
 if __name__ == '__main__':
