4b9366867f3252490af162cf9ec388e2eba8c9ec
BAnderson6895 <BAnderson6895@gmail.com>
2020-03-17 22:08:25 -0400

    Change client usage

--- temperature_monitor_client.py
+++ temperature_monitor_client.py
@@ -7,11 +7,12 @@ def main():
     channel = grpc.insecure_channel('localhost:50051')
     stub = temperature_monitor_pb2_grpc.TemperatureMonitorStub(channel)
     response = stub.CurrentTemperature(temperature_monitor_pb2.Empty())
-    response2 = stub.CurrentTemperature(temperature_monitor_pb2.Empty())
-    response3 = stub.MinMaxTemperature(temperature_monitor_pb2.Empty())
     print("Client received: " + str(response.celsius))
-    print("Client received: " + str(response2.celsius))
-    print("Client received min: " + str(response3.min) + " max: " + str(response3.max))
+    response2 = stub.CurrentTemperature(temperature_monitor_pb2.Empty())
+    print("Client received: " + str(response.celsius))
+    response3 = stub.MinMaxTemperature(temperature_monitor_pb2.Empty())
+    print("Client received: " + str(response.min))
+    print("Client received: " + str(response.max))
 
 
 if __name__ == '__main__':
