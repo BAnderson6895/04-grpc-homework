0e77ca8c25d9467fd032e654f5e8f743c74ea3e3
BAnderson6895 <BAnderson6895@gmail.com>
2020-03-17 21:15:50 -0400

    Fix stub error in client

--- temperature_monitor_client.py
+++ temperature_monitor_client.py
@@ -5,7 +5,7 @@ import temperature_monitor_pb2_grpc
 
 def main():
     with grpc.insecure_channel('localhost:50051') as channel:
-        stub = temperature_monitor_pb2_grpc.ProductInfoStub(channel)
+        stub = temperature_monitor_pb2_grpc.TemperatureMonitorStub(channel)
         response = stub.CurrentTemperature(temperature_monitor_pb2.Empty())
         print("Client received: " + response.message)
 
